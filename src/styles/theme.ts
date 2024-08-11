da9c58c3081e1dd58cb8567fe6438c1f26a4bf96
Luiz Carlos Rocha Correa <luiz.correa.dev@gmail.com>
2024-08-11 22:02:57 +0100

    feat: add new colors

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -7,6 +7,9 @@ const palette = {
   fullWhite: "#FFFFFF",
   darkBg: "#181A20",
   darkSubBg: "#222333",
+  blue: "#009BFF",
+  greenDark: "#97B38A",
+  lightGray: "#F4F4F5",
 };
 
 const theme = createTheme({
@@ -14,8 +17,11 @@ const theme = createTheme({
     background: palette.fullWhite,
     primary: palette.brandAccent,
     secondary: palette.mediumBlack,
+    lightSecondary: palette.lightGray,
     black: palette.fullBlack,
     white: palette.fullWhite,
+    blue: palette.blue,
+    greenDark: palette.greenDark,
   },
   spacing: {
     s: 8,
@@ -27,42 +33,52 @@ const theme = createTheme({
     header: {
       fontFamily: "Nunito-Bold",
       fontSize: 28,
+      fontWeight: "bold",
     },
     "paragraph-one-bold": {
       fontFamily: "Nunito-Bold",
       fontSize: 16,
+      fontWeight: "bold",
     },
     "paragraph-one-semibold": {
       fontFamily: "Nunito-SemiBold",
       fontSize: 16,
+      fontWeight: "semibold",
     },
     "paragraph-one-regular": {
       fontFamily: "Nunito-Regular",
       fontSize: 16,
+      fontWeight: "regular",
     },
     "paragraph-two-bold": {
       fontFamily: "Nunito-Bold",
       fontSize: 15,
+      fontWeight: "bold",
     },
     "paragraph-two-semibold": {
       fontFamily: "Nunito-SemiBold",
       fontSize: 15,
+      fontWeight: "semibold",
     },
     "paragraph-two-regular": {
       fontFamily: "Nunito-Regular",
       fontSize: 15,
+      fontWeight: "regular",
     },
     "paragraph-three-semibold": {
       fontFamily: "Nunito-Regular",
       fontSize: 14,
+      fontWeight: "regular",
     },
     "default-semibold": {
       fontSize: 13,
       fontFamily: "Nunito-SemiBold",
+      fontWeight: "semibold",
     },
     defaults: {
       fontSize: 13,
       fontFamily: "Nunito-Regular",
+      fontWeight: "regular",
     },
   },
 });
