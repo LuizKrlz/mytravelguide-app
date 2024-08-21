956b69f03a2dfa2863a4bdeefc51013b3f02577d
Luiz Carlos Rocha Correa <luiz.correa.dev@gmail.com>
2024-08-21 08:56:03 +0100

    fix: add new itens in atoms

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,6 +1,9 @@
+import { Box } from "./Box";
+import { Button } from "./Button";
+import { ButtonIcon } from "./ButtonIcon";
+import { Input } from "./Input";
 import { Link } from "./Link";
 import * as Icon from "./icons";
-import { Button } from "./Button";
-import { Box } from "./Box";
+import { Typography } from "./Typography";
 
-export { Icon, Link, Box, Button };
+export { Icon, Link, Box, Button, ButtonIcon, Input, Typography };
